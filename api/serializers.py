1dc35297264229c4c2bf26c6825c1f6aa30c3170
hamidbinameziane <abuabdellahhamid@gmail.com>
2026-02-20 18:07:21 +0100

    photo storage6

--- api/serializers.py
+++ api/serializers.py
@@ -8,20 +8,9 @@ class ClientSerializer(serializers.ModelSerializer):
 
 class InterventionSerializer(serializers.ModelSerializer):
     # Ce champ sera utilisé par Flutter pour LIRE l'URL propre
-    imageUrl = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Intervention
-        # 'image' est le champ brut (pour l'upload)
-        # 'imageUrl' est le champ formaté (pour l'affichage Flutter)
-        fields = ['id', 'title', 'description', 'status', 'image', 'imageUrl']
-        # On rend 'image' invisible à la lecture mais disponible à l'écriture
-        extra_kwargs = {'image': {'write_only': True}}
-
-    def get_imageUrl(self, obj):
-        if obj.image:
-            # On récupère l'URL Cloudinary et on force le HTTPS
-            url = obj.image.url
-            return url.replace('http://', 'https://')
-        return None
+        fields = '__all__'
         
