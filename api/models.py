e6f242595c5bea8bb40780f7cdfea84613d25ecd
hamidbinameziane <abuabdellahhamid@gmail.com>
2026-02-20 21:34:05 +0100

    remove photos

--- api/models.py
+++ api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
+import cloudinary.uploader
 
 class Client(models.Model):
     nom = models.CharField(max_length=100)
@@ -27,4 +30,24 @@ class Intervention(models.Model):
     def __str__(self):
         return f"{self.titre} - {self.statut}"
     
+# 1. Supprimer l'image de Cloudinary quand on supprime l'objet Intervention
+@receiver(post_delete, sender=Intervention)
+def delete_image_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        cloudinary.uploader.destroy(instance.image.public_id)
+
+# 2. Supprimer l'ancienne image quand on la remplace par une nouvelle
+@receiver(pre_save, sender=Intervention)
+def delete_old_image_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_image = Intervention.objects.get(pk=instance.pk).image
+    except Intervention.DoesNotExist:
+        return False
+
+    new_image = instance.image
+    if old_image and old_image != new_image:
+        cloudinary.uploader.destroy(old_image.public_id)
 
