f17a762045129c444bdc6d4412c29f6b2b77a160
hamidbinameziane <abuabdellahhamid@gmail.com>
2026-02-20 14:13:24 +0100

    photo storage2

--- core/settings.py
+++ core/settings.py
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dpkg0rvh0',
-    'API_KEY': '569337394668732',
-    'API_SECRET': 'MHwA4DXYrWbvn3Q9sgBecwIxYPc',
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
