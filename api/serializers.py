534e0bf9178f71beaca538535e6cb27ff48de936
hamidbinameziane <abuabdellahhamid@gmail.com>
2026-02-20 18:13:39 +0100

    photo storage7

--- api/serializers.py
+++ api/serializers.py
@@ -7,10 +7,25 @@ class ClientSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class InterventionSerializer(serializers.ModelSerializer):
-    # Ce champ sera utilisé par Flutter pour LIRE l'URL propre
-    image = serializers.SerializerMethodField()
+    # On crée un champ virtuel pour l'URL propre que Flutter va lire
+    imageUrl = serializers.SerializerMethodField()
 
     class Meta:
         model = Intervention
-        fields = '__all__'
+        # 'image' est le champ réel du modèle (pour l'upload)
+        # 'imageUrl' est le champ calculé (pour l'affichage)
+        fields = ['id', 'title', 'description', 'status', 'image', 'imageUrl']
+        
+        # On dit à Django que 'image' sert à l'écriture (upload) 
+        # mais qu'on ne veut pas le voir dans le JSON de réponse
+        extra_kwargs = {
+            'image': {'write_only': True}
+        }
+
+    # Cette méthode DOIT s'appeler get_<nom_du_champ>
+    def get_imageUrl(self, obj):
+        if obj.image:
+            # On récupère l'URL de Cloudinary et on sécurise en HTTPS
+            return obj.image.url.replace('http://', 'https://')
+        return None
         
