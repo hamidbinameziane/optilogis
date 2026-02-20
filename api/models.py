e5ba8e901425783acb5c5c58b6cbb4d259cd3438
hamidbinameziane <abuabdellahhamid@gmail.com>
2026-02-20 14:41:34 +0100

    photo storage3

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Client(models.Model):
     nom = models.CharField(max_length=100)
@@ -20,7 +21,7 @@ class Intervention(models.Model):
     statut = models.CharField(max_length=20, choices=STATUT_CHOICES, default='A_FAIRE')
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     technicien = models.ForeignKey(User, on_delete=models.CASCADE, related_name='missions')
-    image = models.ImageField(upload_to='interventions/', null=True, blank=True)
+    image =CloudinaryField('image', folder='interventions', null=True, blank=True)
     date_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
